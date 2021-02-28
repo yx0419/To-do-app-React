d2651b31f26dc85b39cfdef181b81bb58e1811b3
Yunjae Cho <yunxics@gmail.com>
2021-02-27 22:36:04 -0800

    Created to-do app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <label>What needs to be done?</label>
+      <br />
+
+      <input type="text" name="textinput"></input>
+      <br />
+
+      <button className="btn btn__primary">Add</button>
+      <br />
+
+      <button className="btn toggle-btn" aria-pressed="true">
+        <span>All</span>
+      </button>
+
+      <button className="btn toggle-btn" aria-pressed="false">
+        <span>Active</span>
+      </button>
+
+      <button type="button" className="btn toggle-btn" aria-pressed="false">
+        <span>Completed</span>
+      </button>
+
+      <h2>2 tasks remaining</h2>
+
+      <ul>
+        <li>
+          <input type="checkbox"></input>
+          <span>Eat</span>
+
+          <div>
+            <button className="btn">Edit</button>
+            <button className="btn btn__danger">Delete</button>
+          </div>
+        </li>
+
+        <li>
+          <input type="checkbox"></input>
+          <span>Sleep</span>
+
+          <div>
+            <button className="btn">Edit</button>
+            <button className="btn btn__danger">Delete</button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default App; //exports a function called 'App', which is a component. src/index.js imports this 'App'.
